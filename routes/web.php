5da56f0391564db08ddcd07b864cbc17884a5457
Hamza-bit-lab <hamzarashid7574@gmail.com>
2026-04-28 16:08:53 +0300

    added comment

--- routes/web.php
+++ routes/web.php
@@ -38,6 +38,7 @@ Route::get('/assign-permission-to-user', function(){
 });
 
 Route::middleware('auth')->group(function () {
+    // profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
